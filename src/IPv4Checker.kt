4f8f8266dcd5bac7cc8cc4335e1f06a99b70e37e
shahad-97 <shos8406@gmail.com>
2025-04-04 22:18:04 +0300

    Initial commit

---
+++ src/IPv4Checker.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val testCases = listOf(
+        "192.168.0.1" to true,
+        "256.100.50.25" to false,
+        "192.168.01.1" to false,
+        "192.168.0" to false,
+        "192.168.0.256" to false
+    )
+
+    for ((ip, expected) in testCases) {
+        val result = isValidIPv4(ip)
+        println("Testing $ip: Expected $expected, got $result")
+    }
+}
